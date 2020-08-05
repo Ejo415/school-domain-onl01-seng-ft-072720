2d2447b378aad4f996e1c38e16892d127d0396aa
Eugene O'Neil <ejo7220@gmail.com>
2020-08-05 18:27:16 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -14,7 +14,7 @@ class School
     if roster.include?(grade) == false
       @roster[grade] = []
     end
-    @roster[grade] << name 
+    @roster[grade] << student  
   end
   
   def grade(number)
