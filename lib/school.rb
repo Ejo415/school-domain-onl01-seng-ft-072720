2ecfc91ab3959d08a786072ed2d610399f4f5c51
Eugene O'Neil <ejo7220@gmail.com>
2020-08-05 18:25:43 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -9,5 +9,11 @@ class School
   end
   
   def add_student(student, grade)
+    @student_name = student
+    @grade = grade
+    if roster.include?(grade) == false
+      @roster[grade] = []
+    end
+    @roster[grade] << name 
   end
 end
